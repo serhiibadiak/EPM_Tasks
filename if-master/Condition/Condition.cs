8da098e49b1b1b663566ca85be3060fa2d9a8f06
serhiibadiak <86836345+serhiibadiak@users.noreply.github.com>
2021-07-15 19:22:42 +0300

    Add files via upload

---
+++ if-master/Condition/Condition.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace Condition
+{
+    public static class Condition
+    {
+        /// <summary>
+        /// Implement code according to description of  task 1
+        /// </summary>        
+        public static int Task1(int n)
+        {
+            if (n > 0)
+            {
+                return n * n;
+            }
+            else if (n == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return -n;
+            }
+        }
+
+        /// <summary>
+        /// Implement code according to description of  task 2
+        /// </summary>  
+        public static int Task2(int n)
+        {
+            //TODO :Delete  line  below and write your own solution 
+            throw new NotImplementedException();
+        }
+    }
+}
